087bf31e1a907d2cc0e14d1606486d4552914f41
Fernando <flopezramirez@hotmail.com>
2020-07-08 14:22:05 -0500

    Custom Admin Panel (2)

--- bussinessWebsite/blog/admin.py
+++ bussinessWebsite/blog/admin.py
@@ -10,5 +10,13 @@ admin.site.register(Category, CategoryAdmin)
 # Post.
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ['created', 'updated']
+    list_display = ['title', 'author', 'published', 'post_categories']
+    ordering = ['author', 'published']
+    search_fields = ['title', 'content', 'author__username', 'categories__name']
+    list_filter = ['author__username', 'categories__name']
+
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by('name')])
+    post_categories.short_description = 'categories'
 
 admin.site.register(Post, PostAdmin)
