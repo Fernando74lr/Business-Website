66af329cc5ae070cf7f8f18cdb47d4b617b63de2
Fernando <flopezramirez@hotmail.com>
2020-07-10 22:41:23 -0500

    Creating the views of the Blog (pt.2)

--- bussinessWebsite/blog/models.py
+++ bussinessWebsite/blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     published = models.DateTimeField(verbose_name="Date of creation", default=now)
     image = models.ImageField(verbose_name="Image", upload_to="blog", null=True, blank=True)
     author = models.ForeignKey(User, verbose_name="Auth", on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, verbose_name="Categories")
+    categories = models.ManyToManyField(Category, verbose_name="Categories", related_name="get_posts")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Date and time of creation")
     updated = models.DateTimeField(auto_now=True, verbose_name="Date and time of last change")
 
--- bussinessWebsite/blog/urls.py
+++ bussinessWebsite/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     # Blog.
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    # Dynamic field.
+    path('category/<int:category_id>/', views.category, name="category")
 ]
