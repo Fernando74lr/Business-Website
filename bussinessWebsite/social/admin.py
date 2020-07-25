1f37bb2764343168eb4ed98dd4398007d662b9af
Fernando <flopezramirez@hotmail.com>
2020-07-24 23:46:51 -0500

    Custom Admin Panel 4 (groups)

--- bussinessWebsite/social/admin.py
+++ bussinessWebsite/social/admin.py
@@ -5,4 +5,12 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ['created', 'updated']
 
+    def get_readonly_fields(self, request, obj=None):
+        # Check if the user belongs to the "Staff" group.
+        if request.user.groups.filter(name="Staff").exists():
+            return ['key', 'name']
+        else:
+            return ['created', 'updated']
+        
+
 admin.site.register(Link, LinkAdmin)
